e555c05a90d1f7344439a3066ff7bd1e9f926a77
LuHer10 <lucas.herrera.eckert@gmail.com>
2026-06-15 23:28:27 +0200

    new mit test code

--- code/steadywin/example_mit.cpp
+++ code/steadywin/example_mit.cpp
@@ -27,33 +27,9 @@ int main() {
 
     can.setNonBlocking(true);
 
+    float kp = 15.0f;
+    float kd = 1.2f;
 
-    /*    
-    fl32u8 pos;
-    pos.fl = 0.0f;
-    struct can_frame recv_frame;
-    while (true) {
-        struct can_frame frame;
-        frame.can_id = (11 << 5) | 0x00C;
-        frame.can_dlc = 8;
-        frame.data[0] = pos.u8[0];
-        frame.data[1] = pos.u8[1];
-        frame.data[2] = pos.u8[2];
-        frame.data[3] = pos.u8[3];
-        frame.data[4] = 0x00;
-        frame.data[5] = 0x00;
-        frame.data[6] = 0x00;
-        frame.data[7] = 0x00;
-
-        if (can.sendFrame(frame)) {
-            std::cout << "Frame sent\n";
-            break;
-        }
-    }
-    */
-    
-
-    /* */
     GIM6010 m1(&can, 11);
     m1.start_motor();
     //std::this_thread::sleep_for(std::chrono::milliseconds(10));
@@ -70,7 +46,7 @@ int main() {
     
     m1.stop_motor();
     /**/
-    /*
+    
     GIM6010 m1(&can, 11);
     m1.start_motor();
     //std::this_thread::sleep_for(std::chrono::milliseconds(10));
@@ -78,16 +54,16 @@ int main() {
     m1.setMITMode();
     //std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
-    m1.MITControl(0.0f, 10.0f, 0.0f, 20.0f, 1.0f);
-    
+    m1.MITControl(0.0f, 10.0f, 0.0f, kp, kd);
+
     std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-    
-    m1.MITControl(100.0f, 10.0f, 0.0f, 20.0f, 1.0f);
-    
+
+    m1.MITControl(100.0f, 10.0f, 0.0f, kp, kd);
+
     std::this_thread::sleep_for(std::chrono::milliseconds(5000));
     
     m1.stop_motor();
-    */
+    
     return 0;
 }
 
